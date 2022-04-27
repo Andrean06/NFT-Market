887dec8c5a68a9d5fce99b02c1de4f226f1e2431
AdrianusWrehaspati <104477271+AdrianusWrehaspati@users.noreply.github.com>
2022-04-27 19:57:46 +0700

    Update MyCartsFragment.java

--- app/src/main/java/com/example/nftmarket/MyCartsFragment.java
+++ app/src/main/java/com/example/nftmarket/MyCartsFragment.java
@@ -10,6 +10,13 @@ import android.view.ViewGroup;
 
 
 public class MyCartsFragment extends Fragment {
+    
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+    
+    RecyclerView recyclerView;
+    MyCartAdapter cartAdapter;
+    List<MyCartModel> cartModelList;
 
 
     public MyCartsFragment() {
@@ -20,6 +27,31 @@ public class MyCartsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_carts, container, false);
+        View root = inflater.inflate(R.layout.fragment_my_carts, container, attachToRoot false);
+        
+        db = FirebaseFirestore.getInstance();
+        auth = Firebaseauth.getInstance();
+        recyclerView = root.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        
+        cartModeList = new ArrayList<>();
+        cartAdapter = new MyCartAdapter(getActivity(),cartModelList);
+        recyclerView.setAdapter(cartAdapter);
+        
+        db.collection( collectionpath: "AddToCart").document(auth.getCurrentUser().getUid())
+            .collection( collectionpath: "CurrentUser").get().addOnCompleteListener(new OnCompleteListener<QuerrySnapshot>)
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task){
+                if (task.isSuccesful()){
+                    for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()){
+                        MyCartModel cartModel = documentSNapshot.toObject(MyCartModel.class);
+                        cartModelList.add(cartModel);
+                        cartAdapter.notifyDataSetChanged();
+                    }
+                }
+        }
+    })
+        
+        return root;
     }
 }
